f0bc5ebb264b3b20049843c6fd7aa97d170c9b9e
unknown <utubkanal136@gmail.com>
2026-06-14 23:38:13 +0300

    Add Railway config

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-SECRET_KEY = 'production'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'production')
 
-MYSQL_HOST     = 'localhost'
-MYSQL_USER     = 'root'
-MYSQL_PASSWORD = 'admin'
-MYSQL_DB       = 'library'
+MYSQL_HOST     = os.environ.get('MYSQL_HOST',     'localhost')
+MYSQL_USER     = os.environ.get('MYSQL_USER',     'root')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
+MYSQL_DB       = os.environ.get('MYSQL_DB',       'library')
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                               'static', 'uploads', 'covers')
